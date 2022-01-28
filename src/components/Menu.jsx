f4d087c1b3002b52e7bdc812257fe01e5921387a
Jonas Penaso <edciljonas15@yahoo.com>
2022-01-28 09:37:59 -0800

    adding image uploader

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import Restaurant from "../photo/Restaurant.png";
 
 function Menu() {
   return (
     <>
       <div className="container">
         <div id="order">
-          <div className="restaurant">
-            <img src={Restaurant} alt="restaurant" />
-          </div>
+          <div className="restaurant"></div>
         </div>
       </div>
     </>
